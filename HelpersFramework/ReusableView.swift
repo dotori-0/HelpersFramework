830532251ab746159ffe21af40f557499414493a
dotori-0 <sesan131@gmail.com>
2022-08-16 22:07:33 +0900

    Initial Commit

---
+++ HelpersFramework/ReusableView.swift
@@ -0,0 +1,18 @@
+//
+//  ReusableView.swift
+//  HelpersFramework
+//
+//  Created by SC on 2022/08/16.
+//
+
+import UIKit
+
+public protocol ReusableView {
+    static var reuseIdentifier: String { get }
+}
+
+extension ReusableView {
+    public static var reuseIdentifier: String {
+        return String(describing: Self.self)
+    }
+}
---
+++ HelpersFramework/UIViewController+Extension.swift
@@ -0,0 +1,33 @@
+//
+//  UIViewController+Extension.swift
+//  HelpersFramework
+//
+//  Created by SC on 2022/08/16.
+//
+
+import UIKit
+
+extension UIViewController: ReusableView {
+//    public static var reuseIdentifier: String {
+//        return String(describing: self)
+//    }
+
+    public func showAlert(title: String, message: String, actionTitle: String, actionHandler: @escaping ((UIAlertAction) -> Void)) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+        let cancel = UIAlertAction(title: "취소", style: .cancel)
+        let action = UIAlertAction(title: actionTitle, style: .default, handler: actionHandler)
+        
+        alert.addAction(cancel)
+        alert.addAction(action)
+        
+        self.present(alert, animated: true)
+    }
+}
+
+
+// ??
+public extension UIViewController {
+    
+}
+

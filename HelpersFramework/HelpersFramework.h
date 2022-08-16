31ff74b263c88d98d663f2f1d6ecbe876d250f97
dotori-0 <sesan131@gmail.com>
2022-08-16 21:29:01 +0900

    Initial Commit

---
+++ HelpersFramework/HelpersFramework.h
@@ -0,0 +1,18 @@
+//
+//  HelpersFramework.h
+//  HelpersFramework
+//
+//  Created by SC on 2022/08/16.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HelpersFramework.
+FOUNDATION_EXPORT double HelpersFrameworkVersionNumber;
+
+//! Project version string for HelpersFramework.
+FOUNDATION_EXPORT const unsigned char HelpersFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HelpersFramework/PublicHeader.h>
+
+
